e8d843544dec55f15eea6796332516993714d432
Dario Seminara <darios3@gmail.com>
2011-12-27 20:14:03 -0300

    fixed lambda on tests

--- lib/picotest/autotest.rb
+++ lib/picotest/autotest.rb
@@ -3,7 +3,7 @@ module Picotest
 fixt("fixture should accept :test method", [] => lambda{|x| x.respond_to? :test}).test(method(:fixt))
 
 fixt("raise Fail when tested method raises Fail", 
-  [1] => _raise(Fail) ).test(lambda{raise Fail})
+  [1] => _raise(Fail) ).test(lambda{|x|raise Fail})
 
 failing_fixt = fixt("fixture should accept :test method", [] => lambda{|x| false})
 fixt("test should raise Fail when condition returns false", 
