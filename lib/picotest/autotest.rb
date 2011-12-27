895636a388b86e38dca426f4869971d075364c50
Dario Seminara <darios3@gmail.com>
2011-12-27 20:57:26 -0300

    +1 failing test: wrong message should return fail

--- lib/picotest/autotest.rb
+++ lib/picotest/autotest.rb
@@ -12,5 +12,10 @@ fixt("test should raise Fail when condition returns false",
 fixt("message of Fail should be 'Test fail: always fails' when expectation title is 'always fails'", 
   [lambda{}] => _raise(Fail, "Test fail: always fails") ).test(failing_fixt.method(:test))
 
+failing_fixt = fixt("unexpected message", [] => lambda{|x| false})
+message_fixture = fixt("Should raise Fail with expected message", [lambda{}] => _raise(Fail, "Test fail: expected message") )
+
+fixt("wrong fail message should raise fail", [failing_fixt.method(:test)] => _raise(Fail)).test message_fixture.method(:test) 
+
 end
 
