c4c04aef8cc43b45e5fa970ffd6ab9ea2b4837ab
Dario Seminara <darios3@gmail.com>
2011-12-27 19:58:32 -0300

    +2 test fail: exception assert of Picotest::Fail

--- lib/picotest/autotest.rb
+++ lib/picotest/autotest.rb
@@ -3,5 +3,12 @@ module Picotest
 
 fixt("fixture should accept :test method", [] => lambda{|x| x.respond_to? :test}).test(method(:fixt))
 
+fixt("raise Fail when tested method raises Fail", 
+  [1] => _raise(Fail) ).test(lambda{raise Fail})
+
+failing_fixt = fixt("fixture should accept :test method", [] => lambda{|x| false})
+fixt("test should raise Fail when condition returns false", 
+  [lambda{}] => _raise(Fail) ).test(failing_fixt.method(:test))
+
 end
 
