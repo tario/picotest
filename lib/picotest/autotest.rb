5ad235b2bb4f8324bfaeea8fb73d6bdbeff3b818
Dario Seminara <darios3@gmail.com>
2011-12-27 20:36:02 -0300

    fixed assert name on fixt

--- lib/picotest/autotest.rb
+++ lib/picotest/autotest.rb
@@ -5,7 +5,7 @@ fixt("fixture should accept :test method", [] => lambda{|x| x.respond_to? :test}
 fixt("raise Fail when tested method raises Fail", 
   [1] => _raise(Fail) ).test(lambda{|x|raise Fail})
 
-failing_fixt = fixt("fixture should accept :test method", [] => lambda{|x| false})
+failing_fixt = fixt("always fails", [] => lambda{|x| false})
 fixt("test should raise Fail when condition returns false", 
   [lambda{}] => _raise(Fail) ).test(failing_fixt.method(:test))
 
