ab1edda87532b5a36bbebd5ebc256c2b2305a944
Dario Seminara <darios3@gmail.com>
2011-12-27 21:56:07 -0300

    +2 passing tests: sample tests

--- lib/picotest/autotest.rb
+++ lib/picotest/autotest.rb
@@ -19,6 +19,8 @@ along with picotest.  if not, see <http://www.gnu.org/licenses/>.
 
 =end
 # pico test testing himself
+require "picotest/autotest/samples"
+
 module Picotest
 fixt("fixture should accept :test method", [] => lambda{|x| x.respond_to? :test}).test(method(:fixt))
 
---
+++ lib/picotest/autotest/samples.rb
@@ -0,0 +1,38 @@
+=begin
+
+This file is part of the picotest project, http://github.com/tario/picotest
+
+Copyright (c) 2011 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+picotest is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+picotest is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with picotest.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
+module Picotest
+
+# normal test (as you could do the test)
+fixt([1] => 1, [2] => 4, [3] => 9, [4] => 16).test lambda{|x| x*x}
+fixt([1] => 1, [4] => 2, [9] => 3, [16] => 4).test Math.method(:sqrt)
+
+# picotest testing the test
+fixt(
+  [lambda{|x|x*x}] => _not_raise,
+  [lambda{|x|x**2}] => _not_raise,
+  [lambda{|x| case x; when 1; 1; when 2; 4; when 3; 9; when 4; 16; end}] => _not_raise,
+  [lambda{|x| case x; when 1; 1; when 2; 4; when 3; 9; when 4; 15; end}] => _raise(Fail)
+  ).test(
+    fixt([1] => 1, [2] => 4, [3] => 9, [4] => 16).method(:test)
+  )
+
+end
+
