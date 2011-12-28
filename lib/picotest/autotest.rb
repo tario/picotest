470821af7f91dfa7bf171585772fb35bbca22ba3
Dario Seminara <darios3@gmail.com>
2011-12-27 21:05:46 -0300

    added license text to sources

--- lib/picotest/autotest.rb
+++ lib/picotest/autotest.rb
@@ -1,3 +1,23 @@
+=begin
+
+This file is part of the picotest project, http://github.com/tario/picotest
+
+Copyright (c) 2011 Roberto Dario Seminara <robertodarioseminara@gmail.com>
+
+picotest is free software: you can redistribute it and/or modify
+it under the terms of the gnu general public license as published by
+the free software foundation, either version 3 of the license, or
+(at your option) any later version.
+
+picotest is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.  see the
+gnu general public license for more details.
+
+you should have received a copy of the gnu general public license
+along with picotest.  if not, see <http://www.gnu.org/licenses/>.
+
+=end
 # pico test testing himself
 module Picotest
 fixt("fixture should accept :test method", [] => lambda{|x| x.respond_to? :test}).test(method(:fixt))
